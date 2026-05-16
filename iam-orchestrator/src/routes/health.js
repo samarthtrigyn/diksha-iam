2d58081e710ac63359b8ebd7c6e822d5460d9290
Samarth Nigam <samarth.nigam@trigyn.com>
2026-05-17 00:43:15 +0530

    Remove broken web-portal gitlink

--- iam-orchestrator/src/routes/health.js
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-
-const router = Router();
-
-router.get('/health', (_req, res) => res.json({ status: 'ok', service: 'iam-orchestrator' }));
-
-export default router;
--- iam-service/src/routes/sso.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { getSsoUrl } = require('../controllers/sso.controller');
-
-const router = express.Router();
-
-router.get('/sso', getSsoUrl);
-
-module.exports = router;
